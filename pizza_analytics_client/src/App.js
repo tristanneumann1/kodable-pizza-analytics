46e12ffbf0199eb3cc30f3d98618961df8cf3349
Tristan Neumann <tristan.neumann@hotmail.com>
2022-11-06 18:36:17 -0800

    initialised react app

---
+++ pizza_analytics_client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  // get all clients and all consumptions
+  // get current month
+  // get streaks
+  // get max of current month
+
+  return (
+    <div className="App">
+      {/* <ClientList clients="{}"/>
+      <AddClient />
+      <ConsumptionList consumptions="{}" selectedClient="{}"/>
+      <AddConsumption />
+      <MonthlyReport /> */}
+    </div>
+  );
+}
+
+export default App;
